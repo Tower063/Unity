0c22cac0f1fe3abe8aa4e43048d66af0dde31800
hoge <19050026st@edu.fca.ac.jp>
2021-10-30 15:16:47 +0900

    プレイヤー仮追加

---
+++ New Unity Project/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject[,] gameField = new GameObject[6,6];
+    private int[,] playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 6; x++)
+        {
+            for(int y = 0; y < x + 1; y++)
+            {
+                gameField[x, y] = GameObject.Find("Field" + x + y);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/MovePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            transform.position -= new Vector3(0.7f, 1f, 0.7f);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            transform.position -= new Vector3(-0.7f, 1f, 0.7f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.7f, 1f, 0.7f);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0.7f, 1f, 0.7f);
+        }
+    }
+}
